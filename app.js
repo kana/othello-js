dce474d6b01b0a1401f17eec24e7153ff83c9131
Kana Natsuno <dev@whileimautomaton.net>
2013-06-21 18:28:51 +0900

    Define drawGameBoard

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 (function () {
+  var N = 4;
+
+  function drawGameBoard(board) {
+    var ss = [];
+
+    ss.push('<table>');
+    for (var x = 0; x < N; x++) {
+      ss.push('<tr>');
+      for (var y = 0; y < N; y++) {
+        ss.push('<td class="cell">');
+        ss.push('<span class="disc ');
+        ss.push(board[[x, y]]);
+        ss.push('"></span>');
+        ss.push('</td>');
+      }
+      ss.push('</tr>');
+    }
+    ss.push('</table>');
+
+    $('#game-board').html(ss.join(''));
+  }
+
   function resetGame() {
   }
 
