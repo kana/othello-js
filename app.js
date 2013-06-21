ce4699fdca744adc2e2a386e8d32b0714fe9db37
Kana Natsuno <dev@whileimautomaton.net>
2013-06-21 18:33:18 +0900

    Define makeInitialGameBoard

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 (function () {
   var N = 4;
 
+  var EMPTY = 'empty';
+  var WHITE = 'white';
+  var BLACK = 'black';
+
+  function makeInitialGameBoard() {
+    var board = {};
+
+    for (var x = 0; x < N; x++)
+      for (var y = 0; y < N; y++)
+        board[[x, y]] = EMPTY;
+
+    var x2 = x >> 1;
+    var y2 = y >> 1;
+    board[[x2 - 1, y2 - 1]] = WHITE;
+    board[[x2 - 1, y2 - 0]] = BLACK;
+    board[[x2 - 0, y2 - 1]] = BLACK;
+    board[[x2 - 0, y2 - 0]] = WHITE;
+
+    return board;
+  }
+
   function drawGameBoard(board) {
     var ss = [];
 
