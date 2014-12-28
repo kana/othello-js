5e7c71b497a72215aa2123f882d9d68203dd59fb
Kana Natsuno <dev@whileimautomaton.net>
2014-12-28 20:35:08 +0900

    Implement Monte Carlo Tree Search

--- app.js
+++ app.js
@@ -358,6 +358,97 @@ var othello = {};
 
 
 
+  // Monte Carlo Tree Search {{{1
+
+  function tryMonteCarloTreeSearch(rootGameTree, maxTries) {
+    var root = new Node(rootGameTree);
+
+    for (var i = 0; i < maxTries; i++) {
+      var node = root;
+
+      while (node.untriedMoves.length == 0 && node.childNodes.length != 0)
+        node = node.selectChild();
+
+      if (node.untriedMoves.length != 0)
+        node = node.expandChild();
+
+      var won = node.simulate(rootGameTree.player);
+
+      node.backpropagate(won);
+    }
+
+    var vs = root.childNodes.map(function (n) {return n.visits;});
+    return root.childNodes[vs.indexOf(Math.max.apply(null, vs))].move;
+  }
+
+  function Node(gameTree, parentNode, move) {
+    this.gameTree = gameTree;
+    this.parentNode = parentNode;
+    this.move = move;
+    this.childNodes = [];
+    this.wins = 0;
+    this.visits = 0;
+    this.untriedMoves = gameTree.moves.slice();
+  }
+
+  Node.prototype.selectChild = function () {
+    var totalVisits = this.visits;
+    var values = this.childNodes.map(function (n) {
+      return n.wins / n.visits +
+             Math.sqrt(2 * Math.log(totalVisits) / n.visits);
+    });
+    return this.childNodes[values.indexOf(Math.max.apply(null, values))];
+  };
+
+  Node.prototype.expandChild = function () {
+    var i = Math.floor(Math.random() * this.untriedMoves.length);
+    var move = this.untriedMoves.splice(i, 1)[0];
+    var child = new Node(force(move.gameTreePromise), this, move);
+    this.childNodes.push(child);
+    return child;
+  };
+
+  Node.prototype.simulate = function (player) {
+    var gameTree = this.gameTree;
+    while (gameTree.moves.length != 0) {
+      var i = Math.floor(Math.random() * gameTree.moves.length);
+      gameTree = force(gameTree.moves[i].gameTreePromise);
+    }
+    return 0 < makeScoreBoardWith(weightTables.simpleCount)(
+      gameTree.board,
+      player
+    );
+  };
+
+  Node.prototype.backpropagate = function (result) {
+    for (var node = this; node != null; node = node.parentNode)
+      node.update(result);
+  };
+
+  Node.prototype.update = function (won) {
+    this.wins += won;
+    this.visits += 1;
+  };
+
+  Node.prototype.visualize = function (indent) {
+    indent = indent || 0;
+    var ss = [];
+    ss.push('\n');
+    for (var i = 0; i < indent; i++)
+      ss.push('| ');
+    ss.push('W='); ss.push(this.wins);
+    ss.push('/');
+    ss.push('V='); ss.push(this.visits);
+    ss.push('/');
+    ss.push('U='); ss.push(this.untriedMoves.length);
+    for (var i = 0; i < this.childNodes.length; i++)
+      ss.push(this.childNodes[i].visualize(indent + 1));
+    return ss.join('');
+  };
+
+
+
+
   // API {{{1
 
   var externalAITable = {};
