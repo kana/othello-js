c35e134fe217308800ec9601c6e998d19a90b981
Kana Natsuno <dev@whileimautomaton.net>
2015-02-18 00:13:20 +0900

    Add dummy data for GameList

--- othello-online.js
+++ othello-online.js
@@ -14,7 +14,13 @@ angular.module('OthelloOnline', ['ngRoute', 'firebase'])
     });
 })
 .controller('GameList', function ($scope) {
-  // TODO
+  // TODO: Fetch recent games from Firebase.
+  $scope.games = [
+    {id: 4, black: 'George', white: 'Hans', state: 'preparing', created_at: '2015-02-18T00:12:46+09:00'},
+    {id: 3, black: 'Eve', white: 'Fiona', state: 'preparing', created_at: '2015-02-18T00:11:39+09:00'},
+    {id: 2, black: 'Chris', white: 'Dave', state: 'finished', created_at: '2015-02-18T00:10:00+09:00'},
+    {id: 1, black: 'Alice', white: 'Bob', state: 'playing', created_at: '2015-02-18T00:09:51+09:00'}
+  ];
 })
 .controller('GameDetail', function ($scope) {
   // TODO
