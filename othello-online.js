56c4944c0f9ec6957c002a10f0ecae9bd2d0f96a
Kana Natsuno <dev@whileimautomaton.net>
2015-02-17 23:57:30 +0900

    Add templates to sketch an online game

---
+++ othello-online.js
@@ -0,0 +1,11 @@
+angular.module('OthelloOnline', ['ngRoute', 'firebase'])
+.config(function ($routeProvider) {
+  // TODO
+})
+.controller('GameList', function ($scope) {
+  // TODO
+})
+.controller('GameDetail', function ($scope) {
+  // TODO
+});
+// vim: expandtab softtabstop=2 shiftwidth=2 foldmethod=marker
