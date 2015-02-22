fbeb270643343e8659326f6f4c8fbb0d14f213ab
Kana Natsuno <dev@whileimautomaton.net>
2015-02-23 02:12:49 +0900

    Fetch game outlines from Firebase

--- othello-online.js
+++ othello-online.js
@@ -24,7 +24,12 @@ angular.module('OthelloOnline', ['ngRoute', 'firebase'])
   $routeProvider
     .when('/games', {
       controller: 'GameList',
-      templateUrl: 'othello-online-game-list.html'
+      templateUrl: 'othello-online-game-list.html',
+      resolve: {
+        gameOutlines: function (GameOutlines) {
+          return GameOutlines.fetch();
+        }
+      }
     })
     .when('/games/:gameId', {
       controller: 'GameDetail',
@@ -34,15 +39,8 @@ angular.module('OthelloOnline', ['ngRoute', 'firebase'])
       redirectTo: '/games'
     });
 })
-.controller('GameList', function ($scope) {
-  // TODO: Fetch recent games from Firebase.
-  $scope.games = [
-    {id: 5, black: 'Ian', white: null, state: 'preparing', created_at: '2015-02-23T01:43:57+09:00'},
-    {id: 4, black: 'George', white: 'Hans', state: 'preparing', created_at: '2015-02-18T00:12:46+09:00'},
-    {id: 3, black: 'Eve', white: 'Fiona', state: 'preparing', created_at: '2015-02-18T00:11:39+09:00'},
-    {id: 2, black: 'Chris', white: 'Dave', state: 'finished', created_at: '2015-02-18T00:10:00+09:00'},
-    {id: 1, black: 'Alice', white: 'Bob', state: 'playing', created_at: '2015-02-18T00:09:51+09:00'}
-  ];
+.controller('GameList', function ($scope, gameOutlines) {
+  $scope.games = gameOutlines;
 })
 .controller('GameDetail', function ($scope) {
   // TODO: Fetch the details of a game from Firebase.
