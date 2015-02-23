5ae37e242a5ddf89ce09aa3252894bf39f3073a1
Kana Natsuno <dev@whileimautomaton.net>
2015-02-24 00:06:48 +0900

    Remove unused fbUnauth

--- othello-online.js
+++ othello-online.js
@@ -39,11 +39,6 @@ angular.module('OthelloOnline', ['ngRoute', 'firebase'])
     return deferred.promise;
   }
 })
-.service('fbUnauth', function ($firebaseAuth, fbRef) {
-  return function () {
-    $firebaseAuth(fbRef).$unauth();
-  };
-})
 .service('GameOutlines', function ($q, $firebase, fbRef) {
   var self = this;
   self.fetch = function () {
